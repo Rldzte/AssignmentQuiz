136ec74a76223a57868042dbdf4ff25b4442d745
Rldzte <126166069+Rldzte@users.noreply.github.com>
2023-02-23 12:13:16 +0000

    Add files via upload

---
+++ code.js
@@ -0,0 +1,72 @@
+const quizData = [
+  {
+    question: "What is the capital city of Australia?",
+    options: ["Sydney", "Melbourne", "Canberra", "Brisbane"],
+    answer: "Canberra"
+  },
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["Jupiter", "Mars", "Venus", "Saturn"],
+    answer: "Mars"
+  },
+  {
+    question: "What is the largest country by area?",
+    options: ["Russia", "China", "United States", "Canada"],
+    answer: "Russia"
+  },
+  {
+    question: "What is the currency of Japan?",
+    options: ["Yen", "Won", "Dollar", "Euro"],
+    answer: "Yen"
+  },
+  {
+    question: "Which element has the chemical symbol 'Fe'?",
+    options: ["Iron", "Gold", "Silver", "Copper"],
+    answer: "Iron"
+  },
+
+
+  {
+    question: "What is the most abundant gas in the Earth’s atmosphere?",
+    options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Helium"],
+    answer: "Nitrogen"
+  },
+  {
+    question: "Which country’s name means “The Shallows”?",
+    options: ["Bahamas", "Madagascar", "Tasmania", "Argentina"],
+    answer: "Bahamas"
+  },
+  {
+    question: "Which of the Canary Islands is the largest and most populous?",
+    options: ["La Palma", "Lanzarote", "Tenerife", "Gran Canaria"],
+    answer: "Gran Canaria"
+  },
+  {
+    question: "What type of rock is marble?",
+    options: ["Metamorphic", "Igneous", "Granite", "Sedimentary"],
+    answer: "Metamorphic"
+  },
+  {
+    question: "Which colour is the most common on the national flags of the world, featuring on 75% of national flags?",
+    options: ["Green", "White", "Red", "Blue"],
+    answer: "Red"
+  },
+  {
+    question: "The word “ananas” means what, in almost every other major language apart from English?",
+    options: ["Apple", "Banana", "Asparagus", "Pineapple"],
+    answer: "Pineapple"
+  },
+  {
+    question: "During WWII; Utah, Omaha, Gold, Juno and Sword were nicknames for what?",
+    options: ["Submarines", "Airfields", "Tanks", "Beaches"],
+    answer: "Beaches"
+  },
+  {
+    question: "During WWII; Utah, Omaha, Gold, Juno and Sword were nicknames for what?",
+    options: ["Submarines", "Airfields", "Tanks", "Beaches"],
+    answer: "Beaches"
+  },
+];
+
+const question = document.getElementById("question");
+const optionA = document.getElementById
